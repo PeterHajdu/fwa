79bedff25d9657a430160c16e08c28b3516ab559
Peter Hajdu <peter.ferenc.hajdu@gmail.com>
2015-11-14 22:58:17 +0100

    it prints out only changed files

--- fwa.c
+++ fwa.c
@@ -11,17 +11,20 @@ int create_queue();
 size_t get_number_of_files();
 struct kevent* allocate_event_memory(size_t);
 size_t set_up_events_to_watch( struct kevent *, size_t, char*[] );
+void set_output_buffer();
+void handle_events( int, struct kevent*, size_t );
 
 int main(int argc, char *argv[]) {
 	const size_t number_of_files = get_number_of_files(argc);
 	const int queue = create_queue();
-	struct kevent *events_to_monitor = allocate_event_memory(number_of_files);
-	const size_t number_of_events = set_up_events_to_watch( events_to_monitor, number_of_files, argv );
-	(void)queue;
-	(void)number_of_files;
-	(void)number_of_events;
-
-	printf("%s\n%s\n", argv[1], argv[2]);
+	struct kevent *events_to_monitor = allocate_event_memory(
+			number_of_files );
+	const size_t number_of_events = set_up_events_to_watch(
+			events_to_monitor,
+			number_of_files,
+			argv );
+	set_output_buffer();
+	handle_events( queue, events_to_monitor, number_of_events );
 	return 0;
 }
 
@@ -46,27 +49,72 @@ size_t get_number_of_files(int argc) {
 
 struct kevent* allocate_event_memory(size_t number_of_files)
 {
-	struct kevent * const ptr = reallocarray(NULL, number_of_files, sizeof(struct kevent));
+	struct kevent * const ptr = reallocarray(
+			NULL,
+			number_of_files,
+			sizeof( struct kevent ));
 	if ( kevent == NULL )
 		err(2, "Unable to allocate event memory.");
 	return ptr;
 }
 
-size_t set_up_events_to_watch( struct kevent *events, size_t number_of_files, char* argv[] )
+size_t set_up_events_to_watch(
+		struct kevent *events,
+		size_t number_of_files,
+		char* argv[] )
 {
-	static const unsigned int vnode_events = NOTE_DELETE |  NOTE_WRITE | NOTE_EXTEND | NOTE_ATTRIB | NOTE_LINK | NOTE_RENAME | NOTE_REVOKE;
+	static const unsigned int vnode_events =
+		NOTE_DELETE |
+		NOTE_WRITE |
+		NOTE_EXTEND |
+		NOTE_ATTRIB |
+		NOTE_LINK |
+		NOTE_RENAME |
+		NOTE_REVOKE;
 	int i = 0;
 	int event_slot = 0;
 	for( ; i < number_of_files; i++ )
 	{
 		char* const filename = argv[i+1];
-		int event_fd = open( filename, O_RDONLY );
+		const int event_fd = open( filename, O_RDONLY );
 		if (event_fd<0) {
 			fprintf( stderr, "Unable to open file: %s\n", filename );
 			continue;
 		}
-		EV_SET( &events[event_slot], event_fd, EVFILT_VNODE, EV_ADD | EV_CLEAR, vnode_events, 0, filename );
+		EV_SET(
+			&events[event_slot],
+			event_fd,
+			EVFILT_VNODE,
+			EV_ADD | EV_CLEAR,
+			vnode_events,
+			0,
+			filename );
 		event_slot++;
 	}
 	return event_slot;
 }
+
+void set_output_buffer()
+{
+  static char line_buffer[ 512 ];
+  setvbuf(stdout, line_buffer, _IOLBF, sizeof(line_buffer));
+}
+
+void handle_events(
+		int queue,
+		struct kevent* events_to_monitor,
+		size_t number_of_events ) {
+	struct kevent event_data[1];
+	int event_count = kevent(
+			queue,
+			events_to_monitor,
+			number_of_events,
+			event_data,
+			1,
+			NULL );
+	if( event_count < 0 )
+		err( 3, "Error occured while waiting for events." );
+	if( event_count > 0 )
+		printf( "%s\n", (const char*)event_data[0].udata );
+}
+
--- test.sh
+++ test.sh
@@ -5,7 +5,8 @@ output_file="./_test_output"
 run_with_parameters_in_the_background () {
 	echo "./fwa $* > $output_file"
 	./fwa $* > $output_file &
-	return $?
+	sleep 1
+	return 0
 }
 
 run_with_parameters () {
@@ -23,9 +24,10 @@ test_that_it_prints_out_a_single_files_name_if_it_is_changed () {
 	changed_file=`mktemp` || exit 1
 	unchanged_file=`mktemp` || exit 1
 	run_with_parameters_in_the_background "$unchanged_file $changed_file"
-	echo "some text" > $changed_file
+	echo "some test" > $changed_file
 	sleep 1
 	if grep $unchanged_file $output_file; then
+		echo "unchanged file was printed"
 		return 1
 	fi
 
@@ -33,6 +35,7 @@ test_that_it_prints_out_a_single_files_name_if_it_is_changed () {
 		return 0
 	fi
 
+	echo "changed file was not printed"
 	return 1
 }
 
