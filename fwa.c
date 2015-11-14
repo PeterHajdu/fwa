d47b5b1071f5a7b41760f222882380bba6f0242e
Peter Hajdu <peter.ferenc.hajdu@gmail.com>
2015-11-14 20:08:52 +0100

    it prints out usage if no files are defined

--- fwa.c
+++ fwa.c
@@ -1,7 +1,14 @@
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-	sleep( 10 );
+void usage() {
+	printf("usage: fwa <list of files to watch>\n");
+	exit(1);
+}
+
+int main(int argc, char *argv[]) {
+	if (argc < 2)
+		usage();
+	printf("%s\n", argv[1]);
 	return 0;
 }
--- test.sh
+++ test.sh
@@ -1,18 +1,35 @@
 #!/bin/sh
 
-executable=./fwa
+output_file="./_test_output"
 
-file_to_watch=`mktemp` || exit 1
-output=`mktemp` || exit 1
-./fwa $file_to_watch > $output &
-w_pid=$!
+run_with_parameters_in_the_background () {
+	./fwa $* > $output_file &
+	return $?
+}
 
-expected_output=`mktemp` || exit 1
-echo $file_to_watch > $expected_output
+run_with_parameters () {
+	./fwa $* > $output_file
+	return $?
+}
 
-echo "some text" > $file_to_watch
-sleep 1
-kill $w_pid
+test_that_it_prints_out_usage_information_without_files_to_watch () {
+	run_with_parameters
+	grep -i usage $output_file
+	return $?
+}
 
-diff $expected_output $output
+test_that_it_prints_out_a_single_files_name_if_it_is_changed () {
+	file_to_watch=`mktemp` || exit 1
+	expected_output=`mktemp` || exit 1
+	echo $file_to_watch > $expected_output
+	run_with_parameters_in_the_background $file_to_watch
+	echo "some text" > $file_to_watch
+	sleep 1
+	diff $expected_output $output_file
+	return $?
+}
+
+
+test_that_it_prints_out_usage_information_without_files_to_watch &&
+test_that_it_prints_out_a_single_files_name_if_it_is_changed
 
