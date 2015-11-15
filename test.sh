950f5c363832a755013cf717bdb770a7edec2743
Peter Hajdu <peter.ferenc.hajdu@gmail.com>
2015-11-15 11:55:53 +0100

    restructure test infrastructure

--- test.sh
+++ test.sh
@@ -1,86 +1,30 @@
 #!/bin/sh
 
-output_file="./_test_output"
-fwa_pid=0
+red='\033[0;31m'
+green='\033[0;32m'
+no_color='\033[0m'
 
-run_with_parameters_in_the_background () {
-	echo "./fwa $* > $output_file"
-	./fwa $* > $output_file &
-	fwa_pid=$!
-	sleep 1
-	return 0
+color_print () {
+	echo " -> $1$2${no_color}"
 }
 
-kill_background_fwa () {
-	kill $fwa_pid
+failed () {
+	color_print $red "failed"
+	cat $1
+	exit 1
 }
 
-run_with_parameters () {
-	./fwa $* > $output_file
-	return $?
+passed () {
+	color_print $green "passed"
 }
 
-test_that_it_prints_out_usage_information_without_files_to_watch () {
-	run_with_parameters
-	grep -i usage $output_file
-	return $?
-}
-
-test_that_it_prints_out_a_single_files_name_if_it_is_changed () {
-	changed_file=`mktemp` || exit 1
-	unchanged_file=`mktemp` || exit 1
-	run_with_parameters_in_the_background "$unchanged_file $changed_file"
-	echo "some test" > $changed_file
-	sleep 1
-	kill_background_fwa
-	if grep $unchanged_file $output_file; then
-		echo "unchanged file was printed"
-		return 1
-	fi
-
-	if grep $changed_file $output_file; then
-		return 0
-	fi
-
-	echo "changed file was not printed"
-	return 1
-}
-
-test_that_it_prints_out_each_changed_files_name () {
-	file_1=`mktemp` || exit 1
-	file_2=`mktemp` || exit 1
-	run_with_parameters_in_the_background "$file_1 $file_2"
-	echo "some test" > $file_1
-	echo "some test" > $file_2
-	sleep 1
-	kill_background_fwa
-	if grep $file_1 $output_file && grep $file_2 $output_file; then
-		return 0
-	fi
-
-	echo "changed file was not printed"
-	return 1
-}
-
-test_that_it_prints_out_fast_file_changes_only_once () {
-	file_1=`mktemp` || exit 1
-	run_with_parameters_in_the_background "$file_1"
-	echo "some test" > $file_1
-	echo "some test" > $file_1
-	sleep 1
-	kill_background_fwa
-	number_of_changes=`grep $file_1 $output_file | wc -l`
-	echo "Number of changes: $number_of_changes"
-	if [ $number_of_changes -lt 2 ]; then
-		return 0
-	fi
-
-	echo "Changed file was reported more than once."
-	return 1
-}
-
-test_that_it_prints_out_usage_information_without_files_to_watch &&
-test_that_it_prints_out_a_single_files_name_if_it_is_changed &&
-test_that_it_prints_out_each_changed_files_name &&
-test_that_it_prints_out_fast_file_changes_only_once
+test_names=`grep ^test_ ./test_definitions.sh | cut -f1 -d " "`
+. ./test_definitions.sh
+for tst in $test_names; do
+	echo -n " -> $tst"
+	output_file="$tst.out"
+	eval $tst >$output_file 2>&1 || failed $output_file
+	rm $output_file
+	passed
+done
 
---
+++ test_definitions.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+
+output_file="./_test_output"
+fwa_pid=0
+
+run_with_parameters_in_the_background () {
+	echo "./fwa $* > $output_file"
+	./fwa $* > $output_file &
+	fwa_pid=$!
+	sleep 1
+	return 0
+}
+
+kill_background_fwa () {
+	kill $fwa_pid
+}
+
+run_with_parameters () {
+	./fwa $* > $output_file
+	return $?
+}
+
+test_that_it_prints_out_usage_information_without_files_to_watch () {
+	run_with_parameters
+	grep -i usage $output_file
+	return $?
+}
+
+test_that_it_prints_out_a_single_files_name_if_it_is_changed () {
+	changed_file=`mktemp` || exit 1
+	unchanged_file=`mktemp` || exit 1
+	run_with_parameters_in_the_background "$unchanged_file $changed_file"
+	echo "some test" > $changed_file
+	sleep 1
+	kill_background_fwa
+	if grep $unchanged_file $output_file; then
+		echo "unchanged file was printed"
+		return 1
+	fi
+
+	if grep $changed_file $output_file; then
+		return 0
+	fi
+
+	echo "changed file was not printed"
+	return 1
+}
+
+test_that_it_prints_out_each_changed_files_name () {
+	file_1=`mktemp` || exit 1
+	file_2=`mktemp` || exit 1
+	run_with_parameters_in_the_background "$file_1 $file_2"
+	echo "some test" > $file_1
+	echo "some test" > $file_2
+	sleep 1
+	kill_background_fwa
+	if grep $file_1 $output_file && grep $file_2 $output_file; then
+		return 0
+	fi
+
+	echo "changed file was not printed"
+	return 1
+}
+
+test_that_it_prints_out_fast_file_changes_only_once () {
+	file_1=`mktemp` || exit 1
+	run_with_parameters_in_the_background "$file_1"
+	echo "some test" > $file_1
+	echo "some test" > $file_1
+	sleep 1
+	kill_background_fwa
+	number_of_changes=`grep $file_1 $output_file | wc -l`
+	echo "Number of changes: $number_of_changes"
+	if [ $number_of_changes -lt 2 ]; then
+		return 0
+	fi
+
+	echo "Changed file was reported more than once."
+	return 1
+}
+
