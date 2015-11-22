7882ce37dae38a2c2e1784540f66d4be06050d8f
Peter Hajdu <peter.ferenc.hajdu@gmail.com>
2015-11-22 18:10:38 +0100

    add pledge

--- fwa.c
+++ fwa.c
@@ -25,6 +25,7 @@
 
 #include <stdio.h>
 #include <err.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/event.h>
@@ -44,13 +45,13 @@ int main(int argc, char **argv) {
 	const size_t number_of_files = parse_options(argc, argv);
 	const int queue = create_queue();
 	struct kevent *events_to_monitor = allocate_event_memory(
-			number_of_files );
+			number_of_files);
 	const size_t number_of_events = set_up_events_to_watch(
 			events_to_monitor,
 			number_of_files,
-			argv );
+			argv);
 	set_output_buffer();
-	handle_events( queue, events_to_monitor, number_of_events );
+	handle_events(queue, events_to_monitor, number_of_events);
 	return 0;
 }
 
@@ -64,8 +65,8 @@ void usage() {
 
 int create_queue() {
 	const int queue = kqueue();
-	if ( queue < 0 )
-		err( 1, "Unable to create kernel queue." );
+	if (queue < 0)
+		err(1, "Unable to create kernel queue." );
 	return queue;
 }
 
@@ -77,6 +78,8 @@ size_t parse_options(int argc, char* argv[]) {
 		usage();
 	if (number_of_files < 1)
 		usage();
+	if (pledge("stdio rpath", NULL) == -1)
+		err(6, "pledge");
 	return number_of_files;
 }
 
@@ -84,8 +87,8 @@ struct kevent* allocate_event_memory(size_t number_of_files) {
 	struct kevent * const ptr = reallocarray(
 			NULL,
 			number_of_files,
-			sizeof( struct kevent ));
-	if ( ptr == NULL )
+			sizeof(struct kevent));
+	if (ptr == NULL)
 		err(2, "Unable to allocate event memory.");
 	return ptr;
 }
@@ -95,21 +98,20 @@ struct event_descriptor {
 	int numbers_triggered;
 };
 
-struct event_descriptor* create_event_descriptor( const char* filename ) {
-	struct event_descriptor* descriptor =
-		malloc( sizeof( struct event_descriptor ) );
-	if ( descriptor == NULL )
+struct event_descriptor* create_event_descriptor(const char* filename) {
+	struct event_descriptor* descriptor = malloc(sizeof(struct event_descriptor));
+	if (descriptor == NULL)
 		err(4, "Unable to allocate memory for descriptor.");
 	descriptor->filename=filename;
 	descriptor->numbers_triggered=0;
 	return descriptor;
 }
 
-void mark_event( struct event_descriptor* descriptor ) {
+void mark_event(struct event_descriptor* descriptor) {
 	descriptor->numbers_triggered++;
 }
 
-size_t set_up_events_to_watch( struct kevent *events, size_t number_of_files, char* argv[] ) {
+size_t set_up_events_to_watch(struct kevent *events, size_t number_of_files, char* argv[]) {
 	const unsigned int vnode_events =
 		NOTE_DELETE |
 		NOTE_WRITE |
@@ -120,11 +122,11 @@ size_t set_up_events_to_watch( struct kevent *events, size_t number_of_files, ch
 		NOTE_REVOKE;
 	int i = 0;
 	int event_slot = 0;
-	for( ; i < number_of_files; i++ ) {
+	for(; i < number_of_files; i++) {
 		char* const filename = argv[i+1];
-		const int event_fd = open( filename, O_RDONLY );
+		const int event_fd = open(filename, O_RDONLY);
 		if (event_fd<0)
-			err( 5, "Unable to open file: %s", filename );
+			err(5, "Unable to open file: %s", filename);
 		EV_SET(
 			&events[event_slot],
 			event_fd,
@@ -132,51 +134,55 @@ size_t set_up_events_to_watch( struct kevent *events, size_t number_of_files, ch
 			EV_ADD | EV_CLEAR,
 			vnode_events,
 			0,
-			create_event_descriptor( filename ) );
+			create_event_descriptor(filename));
 		event_slot++;
 	}
+
+	if (pledge("stdio", NULL) == -1)
+		err(7, "pledge");
+
 	return event_slot;
 }
 
 void set_output_buffer() {
-  static char line_buffer[ 512 ];
+  static char line_buffer[512];
   setvbuf(stdout, line_buffer, _IOLBF, sizeof(line_buffer));
 }
 
-void report_and_cleanup_events( struct kevent* monitored_events, size_t number_of_events ) {
+void report_and_cleanup_events(struct kevent* monitored_events, size_t number_of_events) {
 	size_t i = 0;
-	for( ; i < number_of_events; i++ )
+	for(; i < number_of_events; i++)
 	{
 		struct event_descriptor* descriptor =
 			monitored_events[i].udata;
-		if ( descriptor->numbers_triggered == 0 )
+		if (descriptor->numbers_triggered == 0)
 			continue;
-		printf( "%s\n", descriptor->filename );
+		printf("%s\n", descriptor->filename);
 		descriptor->numbers_triggered = 0;
 	}
 }
 
-void handle_events( int queue, struct kevent* events_to_monitor, size_t number_of_events ) {
+void handle_events(int queue, struct kevent* events_to_monitor, size_t number_of_events) {
 	struct kevent event_data[1];
 	struct timespec timeout;
 	timeout.tv_sec=0;
 	timeout.tv_nsec=500000000;
-	while ( 1 ) {
+	while (1) {
 		int event_count = kevent(
 				queue,
 				events_to_monitor,
 				number_of_events,
 				event_data,
 				1,
-				&timeout );
-		if( event_count < 0 )
-			err( 3, "Error occured while waiting for events." );
-		if( event_count > 0 ) {
-			mark_event( event_data[0].udata );
+				&timeout);
+		if(event_count < 0)
+			err(3, "Error occured while waiting for events.");
+		if(event_count > 0) {
+			mark_event(event_data[0].udata);
 			continue;
 		}
 
-		report_and_cleanup_events( events_to_monitor, number_of_events );
+		report_and_cleanup_events(events_to_monitor, number_of_events);
 	}
 }
 
